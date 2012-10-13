681982750b0523a61b33d3701d3a8c3f55382ebb
Marcello Barnaba <vjt@openssl.it>
2012-10-14 01:56:49 +0200

    Add the 'I got it' game mode, and a winning path finally :-)

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -26,42 +26,115 @@ $(function () {
     });
   });
 
-  var people = $('.friend');
+  /* Elimination core */
+  var $mode = 'MODE_ELIMINATE';
 
+  var you_lose = function () {
+    alert ('YOO L0SE!');
+
+    if (confirm ('Wanna play again?'))
+      go.restart ();
+    else
+      go.home ();
+  };
+
+  var you_win = function () {
+    alert ('FOR THE WIN!!!11');
+
+    if (confirm ('Wanna play again?'))
+      go.restart ();
+  };
+
+
+  var root = $('body');
+
+  var people = $('.friend');
   people.click (function () {
     var person = $(this);
-
     $.ajax ({
       url: person.data ('url'),
       data: { id: person.data ('id') },
 
       statusCode: {
-        200: function () { // OK, remove it
-          person.fadeOut ();
+        200: function () {
+          $(root).trigger ('guesswho:excluded', [person]);
         },
 
         418: function () { // YOU'RE A TEAPOT
-          // Hide everyone except this one, that is the correct one.
-          //
-          people.filter (':not([id='+person.attr('id')+'])').fadeOut ();
-          alert ('YOO L0SE!');
+          $(root).trigger ('guesswho:guessed', [person]);
+        },
 
-          if (confirm ('Wanna play again?'))
-            restart.go ();
-        }
+        500: oh_so_sorry
       },
-
-      error: oh_so_sorry
     });
   });
 
-  var restart = $('#new-game');
-  restart.go = function () {
-    window.location.href = restart.data ('url');
-  };
+  $(root).bind({
+    'guesswho:excluded': function (event, person) {
+      switch($mode) {
+      case 'MODE_ELIMINATE':
+        // OK, hide the wrong one
+        person.fadeOut ();
 
+        if (people.filter (':visible').length == 1)
+          you_win ();
+        break;
+
+      case 'MODE_GUESS':
+        // Oh no, you guessed the wrong one ;-)
+        //
+        you_lose ();
+        break;
+      }
+    },
+
+    'guesswho:guessed': function (event, person) {
+      switch($mode) {
+      case 'MODE_ELIMINATE':
+        // Hide everyone except this one, that is the correct one.
+        //
+        people.filter (':not([id='+person.attr('id')+'])').fadeOut ();
+        you_lose ();
+        break;
+
+      case 'MODE_GUESS':
+        you_win ();
+        break;
+      }
+    }
+  });
+
+  var restart = $('#new-game');
   restart.click (function () {
     if (confirm ('Sure, pal?'))
-      restart.go ();
+      go.restart ();
+  });
+
+  var go = {
+    restart: function () {
+      this.go('#restart-url');
+    },
+
+    home: function () {
+      this.go ('#home-url');
+    },
+
+    go: function (elem) {
+      window.location.href = $(elem).val ();
+    }
+  };
+
+
+  var i_got_it = $('#i-got-it');
+  i_got_it.click (function () {
+    if ($mode == 'MODE_ELIMINATE') {
+      $mode = 'MODE_GUESS';
+      i_got_it.addClass ('active');
+      i_got_it.val ('GUESSING...');
+    } else {
+      $mode = 'MODE_ELIMINATE';
+      i_got_it.removeClass ('active');
+      i_got_it.val ('I got it!');
+    }
   });
 });
