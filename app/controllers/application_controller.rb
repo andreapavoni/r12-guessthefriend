f3217b9532cca559e347283277558fe4fc419f27
Marcello Barnaba <vjt@openssl.it>
2012-10-13 21:53:34 +0200

    Encapsulate session access through helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_user=(id)
+    session[:user_id] = id
+  end
+
   def current_game
     session[:game] ||= SecureRandom.hex(12)
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env['omniauth.auth'])
-    session[:user_id] = user.id
+    self.current_user = user.id
     redirect_to play_url
   end
 
   def destroy
-    session[:user_id] = nil
+    self.current_user = nil
     redirect_to root_url
   end
 end
