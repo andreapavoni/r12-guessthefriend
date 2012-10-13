330b46a9168dc0b833a5131b45228b3afbbee849
Andrea Pavoni <andrea.pavoni@gmail.com>
2012-10-13 06:59:37 +0200

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guesswho::Application.config.secret_token = 'e5bae74e9f929acbf93a319e9bad0133efbcdaf3093983be11a8f524643080641004d027f6d7596ff895bb28befb9b1c86a5f3d607f7e745d4e4368d401fb851'
