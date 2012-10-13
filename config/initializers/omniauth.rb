c2b4f8224bf6e53361797a3159d73b6734ab1102
Marcello Barnaba <vjt@openssl.it>
2012-10-13 13:29:09 +0200

    Setup permissions to fetch info from friends

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, APP_CONF[:facebook][:app_id], APP_CONF[:facebook][:secret]
+  provider :facebook, APP_CONF[:facebook][:app_id], APP_CONF[:facebook][:secret],
+    scope: 'user_about_me,user_status,friends_activities,friends_about_me,friends_birthday,friends_checkins,friends_events,friends_groups,friends_hometown,friends_interests,friends_likes,friends_relationships,friends_status,friends_work_history'
 end
