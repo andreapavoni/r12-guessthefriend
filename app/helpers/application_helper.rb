8d6fcb9f137c7a1513388b7454d0cab3c3ba19ee
Claudio Floreani <claudio.floreani@gmail.com>
2012-10-14 02:37:33 +0200

    _twitter_button.html.erb: [+] Partial for the twitter button application_helper.rb: [+] helper for the twitter button application.html.erb: [+] displays the twitter button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,12 @@ module ApplicationHelper
   def google_analytics
     render partial: 'shared/google_analytics'
   end
+
   def google_plusone
     render partial: 'shared/google_plusone'
+  end
+
+  def twitter_button
+    render partial: 'shared/twitter_button'
   end
 end
