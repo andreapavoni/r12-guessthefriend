69a6076aef2cb68d08ec2d3eac48ab056e958c05
Andrea Pavoni <andrea.pavoni@gmail.com>
2012-10-14 15:11:55 +0200

    add #profile_pic_for view helper (caches player profile pics)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
     '/auth/facebook'
   end
 
+  def profile_pic_for(fb_uid, oauth_token)
+  Rails.cache.fetch("user/#{fb_uid}/profile_pic", expires_in: 864000, race_condition_ttl: 5) do
+    api = Koala::Facebook::API.new(oauth_token)
+    api.get_object(:me, fields: :picture)['picture']['data']['url']
+    end
+  end
 end
